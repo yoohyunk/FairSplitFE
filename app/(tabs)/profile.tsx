63453d778ed1eb0aa8f2d2c84083de8e369708f1
yoohyunk <yoohyunk20@gmail.com>
2025-05-28 19:48:06 -0600

    add initial screens

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,8 @@
+import { Text, View } from "react-native";
+export default function ProfileScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Profile Screen</Text>
+    </View>
+  );
+}
